b7f26f895c19956d299877c0f6859d19f28f1c2a
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-16 19:14:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,8 @@
 # Write your code here!
+def game_hash
+  hash = {
+    :home => {:team_name => "Brooklyn Nets"},
+            {:colors => ["Black", "White"] },
+            {:players => [{}]
+  }
+end
