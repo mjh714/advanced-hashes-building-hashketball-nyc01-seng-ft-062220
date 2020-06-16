13261e30515f2d1ae15053997b6a5201250ef548
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-16 19:31:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -37,8 +37,19 @@ def game_hash
               :slam_dunks => 15
             },
             {
-              
+              :player_name => "Mason Plumlee",
+              :number => 1,
+              :shoe => 19,
+              :points => 26,
+              :rebounds => 11,
+              :assists => 6,
+              :steals => 3,
+              :blocks => 8,
+              :slam_dunks => 5
             },
+            {
+              
+            }
             ]
     :away => {}
   }
