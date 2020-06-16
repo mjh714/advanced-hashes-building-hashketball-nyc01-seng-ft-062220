4918828e675419f035fa1cad6291437b70617342
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-16 19:27:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,9 @@ def game_hash
               :steals => 3,
               :blocks => 1,
               :slam_dunks => 1
-            }]
+            },
+            {:
+            ]
     :away => {}
   }
 end
