869d2c92c366352f7ddf53fea71c7964b1b8a11a
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-16 19:34:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -65,13 +65,13 @@ def game_hash
         players: [{
               :player_name => "Jeff Adrien",
               :number => 4,
-              :shoe => 16,
-              :points => 22,
-              :rebounds => 12,
-              :assists => 12,
-              :steals => 3,
-              :blocks => 1,
-              :slam_dunks => 1
+              :shoe => 18,
+              :points => 10,
+              :rebounds => 1,
+              :assists => 1,
+              :steals => 2,
+              :blocks => 7,
+              :slam_dunks => 2
             },
             {:player_name => "Reggie Evans",
               :number => 30,
